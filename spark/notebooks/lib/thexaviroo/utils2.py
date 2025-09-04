c6043d8718e245ee10f67cfa77c11bb487bceff3
thexaviroo <figueroaxavier24@gmail.com>
2025-09-04 13:38:27 -0600

    Prep for Lab 01 branch

---
+++ spark/notebooks/lib/thexaviroo/utils2.py
@@ -0,0 +1,12 @@
+def add( x, y ):
+    total = 0
+    for n in args:
+        total += n 
+        return total
+
+def sub( x, y ):
+    total = 0
+    for n in args:
+        total -= n
+        return total
+    
