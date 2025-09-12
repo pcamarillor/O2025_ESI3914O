b944491a10fb292d42a1792bc2f6bb37d1bdba0c
Luis Gonzalez <luisal.gonzaleze@gmail.com>
2025-09-11 19:50:01 -0600

    Generate Schema Function

---
+++ spark/notebooks/lib/luis_gonzalez/spark_utils.py
@@ -0,0 +1,30 @@
+from pyspark.sql.types import (
+    StructType, StructField, StringType, IntegerType, BooleanType, 
+    DoubleType, TimestampType
+)
+
+class SparkUtils:
+    """A helper class for Spark."""
+    _type_lookup = {
+        "StringType": StringType(),
+        "IntegerType": IntegerType(),
+        "BooleanType": BooleanType(),
+        "DoubleType": DoubleType(),
+        "TimestampType": TimestampType(),
+    }
+
+    @staticmethod
+    def generate_schema(columns: list[tuple[str, str]]) -> StructType:
+        fields = []
+        
+        for col_name, col_type_text in columns:
+            
+            spark_type = SparkUtils._type_lookup.get(col_type_text)
+            
+            if spark_type is None:
+                raise ValueError(f"Sorry, I don't know the type '{col_type_text}'!")
+
+            rule = StructField(col_name, spark_type, True)
+            fields.append(rule)
+            
+        return StructType(fields)
