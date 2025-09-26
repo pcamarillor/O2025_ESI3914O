5e5b7577e1fe42b8fb7534357d8152dc6993cb1f
Arantxa Angulo <akrankaa10@gmail.com>
2025-09-25 19:52:53 -0600

    Lab04 and lecture 11 notes

--- spark/notebooks/lib/arantxa/spark_utils.py
+++ spark/notebooks/lib/arantxa/spark_utils.py
@@ -1,5 +1,5 @@
 from urllib.parse import urlparse
-from pyspark.sql.types import StructType, StringType, IntegerType, IntegerType, LongType, ShortType, DoubleType, FloatType, BooleanType, DateType, FloatType, BooleanType, DateType, TimestampType, BinaryType, StructField
+from pyspark.sql.types import StructType, StringType, IntegerType, IntegerType, LongType, ShortType, DoubleType, FloatType, BooleanType, DateType, FloatType, BooleanType, DateType, TimestampType, BinaryType, StructField, ArrayType
 
 def parse_line(line):
     parts = line.strip().split(",")
@@ -38,6 +38,8 @@ class SparkUtils:
             "date": DateType(),
             "timestamp": TimestampType(),
             "binary": BinaryType(),
+            "array_int": ArrayType(IntegerType()),
+            "struct": StructType()
         }
 
         struct_fields = []
